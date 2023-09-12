d2a60effb952e77be6781666b7ad3eea7a743846
Yazuzaki <pgarcia3434@yahoo.com>
2023-09-12 13:51:55 +0800

    fixed the Front end for the tabbing

--- application/views/page/test.php
+++ application/views/page/test.php
@@ -35,9 +35,8 @@
         .tabs {
             position: relative;
             margin: 3rem 0;
-            background: #1abc9c;
             height: auto;
-            /* Adjusted height to fit contents */
+
         }
 
         .tabs::before,
@@ -111,7 +110,7 @@
         <div class="tabs">
             <div class="tab">
                 <input type="radio" name="css-tabs" id="tab-1" checked class="tab-switch">
-                <label for="tab-1" class="tab-label">Tab One</label>
+                <label for="tab-1" class="tab-label">Pending Reservations</label>
                 <div class="tab-content">
 
                     <table id="myTable" class="display">
@@ -162,9 +161,9 @@
             </div>
             <div class="tab">
                 <input type="radio" name="css-tabs" id="tab-2" class="tab-switch">
-                <label for="tab-2" class="tab-label">Tab Two</label>
+                <label for="tab-2" class="tab-label">Today's Reservations</label>
                 <div class="tab-content">
-                    <table id="myTable2" class="display">
+                    <table id="myTable2" class="display table table-striped table-bordered">
                         <thead>
                             <tr>
                                 <th>Reserve ID</th>
@@ -173,7 +172,6 @@
                                 <th>Status</th>
                                 <th>Sport</th>
                                 <th>Court</th>
-                                <th>Action</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -197,24 +195,18 @@
                                     <td>
                                         <?= $row->court ?>
                                     </td>
-                                    <td>
-                                        <a href="#" data-toggle="modal" data-target="#responseModal" data-action="approve"
-                                            data-id="<?= $row->id ?>" class="btn btn-success">Approve</a>
-                                        <a href="#" data-toggle="modal" data-target="#responseModal" data-action="decline"
-                                            data-id="<?= $row->id ?>" class="btn btn-danger">Decline</a>
-                                    </td>
                                 </tr>
                             <?php endforeach; ?>
-
                         </tbody>
                     </table>
                 </div>
             </div>
+
             <div class="tab">
                 <input type="radio" name="css-tabs" id="tab-3" class="tab-switch">
-                <label for="tab-3" class="tab-label">Tab Three</label>
+                <label for="tab-3" class="tab-label">Upcoming Reservations</label>
                 <div class="tab-content">
-                    <table id="myTable3" class="display">
+                    <table id="myTable3" class="display table table-striped table-bordered">
                         <thead>
                             <tr>
                                 <th>Reserve ID</th>
@@ -223,7 +215,6 @@
                                 <th>Status</th>
                                 <th>Sport</th>
                                 <th>Court</th>
-                                <th>Action</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -247,163 +238,153 @@
                                     <td>
                                         <?= $row->court ?>
                                     </td>
-                                    <td>
-                                        <a href="#" data-toggle="modal" data-target="#responseModal" data-action="approve"
-                                            data-id="<?= $row->id ?>" class="btn btn-success">Approve</a>
-                                        <a href="#" data-toggle="modal" data-target="#responseModal" data-action="decline"
-                                            data-id="<?= $row->id ?>" class="btn btn-danger">Decline</a>
-                                    </td>
                                 </tr>
                             <?php endforeach; ?>
-
                         </tbody>
                     </table>
                 </div>
             </div>
-        </div>
-        <p></p>
-    </div>
+            <div class="modal fade" id="responseModal" tabindex="-1" role="dialog" aria-labelledby="responseModalLabel"
+                aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="responseModalLabel">Response</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body" id="responseBody">
 
-    <div class="modal fade" id="responseModal" tabindex="-1" role="dialog" aria-labelledby="responseModalLabel"
-        aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="responseModalLabel">Response</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body" id="responseBody">
-
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <script>
-        $(document).ready(function () {
-            $('#myTable').DataTable();
-            $('#myTable2').DataTable();
-            $('#myTable3').DataTable();
-        });
-
-        document.addEventListener("DOMContentLoaded", function () {
-            const responseModal = document.getElementById('responseModal');
-            const responseBody = document.getElementById('responseBody');
-
-            const approveButtons = document.querySelectorAll(".btn-success[data-action='approve']");
-            const declineButtons = document.querySelectorAll(".btn-danger[data-action='decline']");
-
-
-            approveButtons.forEach(button => {
-                button.addEventListener("click", function () {
-                    const reservationId = this.getAttribute("data-id");
-                    const reservationRow = this.closest("tr");
-                    const reservationDate = new Date(reservationRow.getAttribute("data-date"));
-                    performAction(reservationId, "approve", reservationDate, reservationRow);
+            <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+            <script>
+                
+                $(document).ready(function () {
+                    $('#myTable').DataTable();
+                    $('#myTable2').DataTable();
+                    $('#myTable3').DataTable();
                 });
-            });
 
-            declineButtons.forEach(button => {
-                button.addEventListener("click", function () {
-                    const reservationId = this.getAttribute("data-id");
-                    performAction(reservationId, "decline");
+                document.addEventListener("DOMContentLoaded", function () {
+                    const responseModal = document.getElementById('responseModal');
+                    const responseBody = document.getElementById('responseBody');
+
+                    const approveButtons = document.querySelectorAll(".btn-success[data-action='approve']");
+                    const declineButtons = document.querySelectorAll(".btn-danger[data-action='decline']");
+
+
+                    approveButtons.forEach(button => {
+                        button.addEventListener("click", function () {
+                            const reservationId = this.getAttribute("data-id");
+                            const reservationRow = this.closest("tr");
+                            const reservationDate = new Date(reservationRow.getAttribute("data-date"));
+                            performAction(reservationId, "approve", reservationDate, reservationRow);
+                        });
+                    });
+
+                    declineButtons.forEach(button => {
+                        button.addEventListener("click", function () {
+                            const reservationId = this.getAttribute("data-id");
+                            performAction(reservationId, "decline");
+                        });
+                    });
+
+                    function performAction(reservationId, action, reservationDate, reservationRow) {
+                        fetch(`<?= base_url('Page/') ?>${action}_reservation/${reservationId}`, {
+                            method: "GET",
+                        })
+                            .then(response => response.json())
+                            .then(data => {
+                                if (data.status === "success") {
+                                    responseBody.innerText = "Reservation has been " + action + "d.";
+
+
+                                    if (isToday(reservationDate)) {
+
+                                        if (action === 'approve') {
+                                            reservationRow.remove();
+                                            const todayTable = document.getElementById('today');
+                                            todayTable.appendChild(reservationRow);
+                                        } else if (action === 'decline') {
+
+                                            reservationRow.remove();
+                                        }
+                                    } else {
+
+                                        if (action === 'approve') {
+                                            reservationRow.remove();
+                                            const futureTable = document.getElementById('future');
+                                            futureTable.appendChild(reservationRow);
+                                        } else if (action === 'decline') {
+
+                                            reservationRow.remove();
+                                        }
+                                    }
+
+
+                                    refreshReservationTable();
+                                } else {
+                                    responseBody.innerText = data.message;
+                                }
+                                $('#responseModal').modal('show');
+                            })
+                            .catch(error => {
+                                console.error(error);
+                            });
+                    }
+
+
+                    responseModal.addEventListener('hidden.bs.modal', function () {
+                        responseBody.innerText = '';
+                        location.reload();
+                    });
+
+                    responseModal.querySelector('.btn-secondary').addEventListener('click', function () {
+                        responseBody.innerText = '';
+                        $('#responseModal').modal('hide');
+                        location.reload();
+                    });
+
+                    // AJAX for date filtering
+                    const dateFilterForm = document.getElementById('dateFilterForm');
+                    const reservationTableContainer = document.getElementById('reservationTableContainer');
+
+                    dateFilterForm.addEventListener('submit', function (e) {
+                        e.preventDefault();
+                        const formData = new FormData(dateFilterForm);
+
+                        fetch('<?= site_url('Page/fetch_reservations') ?>', {
+                            method: 'POST',
+                            body: formData
+                        })
+                            .then(response => response.text())
+                            .then(data => {
+                                reservationTableContainer.innerHTML = data; // Update reservation table content
+                            })
+                            .catch(error => {
+                                console.error(error);
+                            });
+                    });
+
+                    // Function to determine if the date is today
+                    function isToday(date) {
+                        const today = new Date();
+                        return date.getDate() === today.getDate() &&
+                            date.getMonth() === today.getMonth() &&
+                            date.getFullYear() === today.getFullYear();
+                    }
+
                 });
-            });
-
-            function performAction(reservationId, action, reservationDate, reservationRow) {
-                fetch(`<?= base_url('Page/') ?>${action}_reservation/${reservationId}`, {
-                    method: "GET",
-                })
-                    .then(response => response.json())
-                    .then(data => {
-                        if (data.status === "success") {
-                            responseBody.innerText = "Reservation has been " + action + "d.";
-
-
-                            if (isToday(reservationDate)) {
-
-                                if (action === 'approve') {
-                                    reservationRow.remove();
-                                    const todayTable = document.getElementById('today');
-                                    todayTable.appendChild(reservationRow);
-                                } else if (action === 'decline') {
-
-                                    reservationRow.remove();
-                                }
-                            } else {
-
-                                if (action === 'approve') {
-                                    reservationRow.remove();
-                                    const futureTable = document.getElementById('future');
-                                    futureTable.appendChild(reservationRow);
-                                } else if (action === 'decline') {
-
-                                    reservationRow.remove();
-                                }
-                            }
-
-
-                            refreshReservationTable();
-                        } else {
-                            responseBody.innerText = data.message;
-                        }
-                        $('#responseModal').modal('show');
-                    })
-                    .catch(error => {
-                        console.error(error);
-                    });
-            }
-
-
-            responseModal.addEventListener('hidden.bs.modal', function () {
-                responseBody.innerText = '';
-                location.reload();
-            });
-
-            responseModal.querySelector('.btn-secondary').addEventListener('click', function () {
-                responseBody.innerText = '';
-                $('#responseModal').modal('hide');
-                location.reload();
-            });
-
-            // AJAX for date filtering
-            const dateFilterForm = document.getElementById('dateFilterForm');
-            const reservationTableContainer = document.getElementById('reservationTableContainer');
-
-            dateFilterForm.addEventListener('submit', function (e) {
-                e.preventDefault();
-                const formData = new FormData(dateFilterForm);
-
-                fetch('<?= site_url('Page/fetch_reservations') ?>', {
-                    method: 'POST',
-                    body: formData
-                })
-                    .then(response => response.text())
-                    .then(data => {
-                        reservationTableContainer.innerHTML = data; // Update reservation table content
-                    })
-                    .catch(error => {
-                        console.error(error);
-                    });
-            });
-
-            // Function to determine if the date is today
-            function isToday(date) {
-                const today = new Date();
-                return date.getDate() === today.getDate() &&
-                    date.getMonth() === today.getMonth() &&
-                    date.getFullYear() === today.getFullYear();
-            }
-
-        });
-    </script>
+            </script>
 </body>
 
 </html>
